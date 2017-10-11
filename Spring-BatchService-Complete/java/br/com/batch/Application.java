988a5e46193e34abacd3ccd90714b6d0e70f2c2d
halysson.h.nobrega <halysson.h.nobrega@accenture.com>
2017-10-10 23:39:05 -0300

    Ajustes no projeto complete e initial

--- Spring-BatchService-Complete/java/br/com/batch/Application.java
+++ Spring-BatchService-Complete/java/br/com/batch/Application.java
@@ -1,12 +1,14 @@
 package br.com.batch;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
 
 @EnableBatchProcessing
 public class Application {
 
 	public static void main(String[] args) {
-
+	     System.exit(SpringApplication.exit(SpringApplication.run(
+	             BatchConfiguration.class, args)));
 	}
 
 }
