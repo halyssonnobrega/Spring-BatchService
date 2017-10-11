ed51ad9b006edeed8858e11e35730a7301238439
halysson.h.nobrega <halysson.h.nobrega@accenture.com>
2017-10-11 00:49:16 -0300

    Ajustes

--- Spring-BatchService-Complete/src/main/java/br/com/batch/BatchConfiguration.java
+++ Spring-BatchService-Complete/src/main/java/br/com/batch/BatchConfiguration.java
@@ -3,6 +3,7 @@ package br.com.batch;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
@@ -13,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@EnableBatchProcessing
 @Configuration
 public class BatchConfiguration {
 
